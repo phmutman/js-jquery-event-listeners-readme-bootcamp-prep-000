c213a3548e1e4442a35d7906f3a9ae77aa09d125
Manzi <phmutman@hotmail.com>
2019-02-13 01:23:00 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,8 +6,8 @@ function getIt(){
 }
 
 function frameIt(){
-  $('img').on("load",function(img){
-    img.addClass("tasty")
+  $("img").on("load",function(img){
+    $("img").addClass("tasty")
     img.style.border ="red"
   })
 }
